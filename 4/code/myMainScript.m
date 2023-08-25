05f5812115618c84ddb483cb8a379d138107c633
wermos <63574588+wermos@users.noreply.github.com>
2023-08-26 04:50:03 +0530

    Added some plot-generating code.

--- 4/code/myMainScript.m
+++ 4/code/myMainScript.m
@@ -1,12 +1,47 @@
 J1 = double(imread("T1.jpg"));
 J2 = double(imread("T2.jpg"));
+J3 = imrotate(J2, 28.5, "bilinear", "crop");
 
-J3 = imrotate(J2, 28.5);
+X = linspace(-45, 45, 91);
 
-for angle = -45:1:45 % start, increment, end
-    J4 = imrotate(J3, angle);
+ncc_values = arrayfun(@NCCHelper, X);
+joint_entropy_values = arrayfun(@JointEntropyHelper, X);
+qmi_values = arrayfun(@QMIHelper, X);
 
-    ncc = normxcorr2(J3, J4)
-    je = 
-    qmi = 
+% plot(X, ncc_values, X, joint_entropy_values, X, qmi_values)
+
+plot(X, ncc_values);
+saveas(gcf, "ncc.png");
+
+plot(X, joint_entropy_values);
+saveas(gcf, "je.png");
+
+plot(X, qmi_values);
+saveas(gcf, "qmi.png");
+
+function ncc = NCCHelper(angle)
+    global J1;
+    global J2;
+    global J3;
+
+    J4 = imrotate(J3, angle, "bilinear", "crop");
+    ncc = NCC(J1, J4);
+end
+
+function je = JointEntropyHelper(angle)
+    global J1;
+    global J2;
+    global J3;
+
+    J4 = imrotate(J3, angle, "bilinear", "crop");
+    je = JointEntropy(J1, J4);
+end
+
+function qmi = QMIHelper(angle)
+    global J1;
+    global J2;
+    global J3;
+
+    J4 = imrotate(J3, angle, "bilinear", "crop");
+    qmi = QMI(J1, J4);
 end
