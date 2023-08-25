195fe932ae470c0eb99ee4583f3d5d5286022755
wermos <63574588+wermos@users.noreply.github.com>
2023-08-26 04:48:08 +0530

    Minor change in NCC implementation.

--- 4/code/NCC.m
+++ 4/code/NCC.m
@@ -6,9 +6,9 @@ function ncc = NCC(I1, I2)
     I2 = I2 - I2_average;
 
     numerator = sum(times(I1, I2), "all");
-    
-    denom1 = sum(times(I1, I1), "all");
-    denom2 = sum(times(I2, I2), "all");
+
+    denom1 = sum(I1.^ 2, "all");
+    denom2 = sum(I2.^ 2, "all");
     
     ncc = abs(numerator / sqrt(denom1 * denom2));
 end
