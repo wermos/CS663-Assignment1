4bf34e7d211c9c98581798f7a25b759f7a0d5133
wermos <63574588+wermos@users.noreply.github.com>
2023-08-26 05:37:56 +0530

    Added some code to generate the JE histogram.

--- 4/code/myMainScript.m
+++ 4/code/myMainScript.m
@@ -8,20 +8,32 @@ ncc_values = arrayfun(@NCCHelper, X);
 joint_entropy_values = arrayfun(@JointEntropyHelper, X);
 qmi_values = arrayfun(@QMIHelper, X);
 
-% plot(X, ncc_values, X, joint_entropy_values, X, qmi_values)
-
 plot(X, ncc_values);
-saveas(gcf, "ncc.png");
+saveas(gcf, "ncc.pdf");
 
 plot(X, joint_entropy_values);
-saveas(gcf, "je.png");
+saveas(gcf, "je.pdf");
 
 plot(X, qmi_values);
-saveas(gcf, "qmi.png");
+saveas(gcf, "qmi.pdf");
+
+[~, index] = min(joint_entropy_values);
+
+% 1st element corresponds to -45 degrees, so -45. We add the index. Then,
+% we subtract 1 because indexing in MATLAB starts at 1.
+best_angle = -45 + index - 1;
+
+closest_image = imrotate(J3, best_angle, "bilinear", "crop");
+
+jh = JointHistogram(J1, J4);
+
+histogram = imagesc(jh)
+colorbar
+
+saveas(histogram, "histogram.pdf");
 
 function ncc = NCCHelper(angle)
     global J1;
-    global J2;
     global J3;
 
     J4 = imrotate(J3, angle, "bilinear", "crop");
@@ -30,7 +42,6 @@ end
 
 function je = JointEntropyHelper(angle)
     global J1;
-    global J2;
     global J3;
 
     J4 = imrotate(J3, angle, "bilinear", "crop");
@@ -39,7 +50,6 @@ end
 
 function qmi = QMIHelper(angle)
     global J1;
-    global J2;
     global J3;
 
     J4 = imrotate(J3, angle, "bilinear", "crop");
