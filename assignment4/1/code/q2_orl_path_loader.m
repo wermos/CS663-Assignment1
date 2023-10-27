0bb8e2436a5cd9e410e6d2fa26194a73df71ac55
wermos <63574588+wermos@users.noreply.github.com>
2023-10-28 05:26:19 +0530

    Wrote some preliminary code for Q2.

---
+++ assignment4/1/code/q2_orl_path_loader.m
@@ -0,0 +1,55 @@
+function [trainingArray, testingArray, galleryImages] = q2_orl_path_loader
+    root_path = "../images/ORL/";
+
+    % constants
+    NUM_PEOPLE = 40;
+    NUM_TRAINING_IMAGES = 6;
+    NUM_TESTING_IMAGES = 4;
+
+    % we only need a 1D array
+    trainingArray = strings(32 * NUM_TRAINING_IMAGES, 1);
+    testingArray = strings(NUM_PEOPLE * NUM_TESTING_IMAGES, 2);
+    galleryImages = strings(32, 1);
+
+    trainingArrayIdx = uint16(1);
+    testingArrayIdx = uint16(1);
+    galleryIdx = uint16(1);
+    for i = 1:NUM_PEOPLE
+        dir_path = append(root_path, "s", int2str(i), "/");
+
+        if i <= 32
+            for j = 1:10
+                full_path = append(dir_path, int2str(j), ".pgm");
+                if j == 1
+                    galleryImages(galleryIdx) = full_path;
+                    galleryIdx = galleryIdx + 1;
+                end
+    
+                if j <= 6
+                    trainingArray(trainingArrayIdx) = full_path;
+                    trainingArrayIdx = trainingArrayIdx + 1;
+                else
+                    testingArray(testingArrayIdx, 1) = full_path;
+                    % we store the corresponding index so that we can use that
+                    % info to check if the system correctly recognized the
+                    % image or not.
+                    testingArray(testingArrayIdx, 2) = int2str(i);
+    
+                    testingArrayIdx = testingArrayIdx + 1;
+                end
+            end
+        else
+            for j = 7:10
+                full_path = append(dir_path, int2str(j), ".pgm");
+            
+                testingArray(testingArrayIdx, 1) = full_path;
+                % we store the corresponding index so that we can use that
+                % info to check if the system correctly recognized the
+                % image or not.
+                testingArray(testingArrayIdx, 2) = int2str(i);
+
+                testingArrayIdx = testingArrayIdx + 1;
+            end
+        end
+    end
+end
---
+++ assignment4/1/code/q2_orl_recognition.m
@@ -0,0 +1,64 @@
+[trainingArray, testingArray, galleryImages] = q2_orl_path_loader();
+
+% Initialize training data array
+[num_training_rows, ~] = size(trainingArray);
+
+trainingData = zeros(92 * 112, num_training_rows);
+
+% load in training data
+for k = 1:num_training_rows
+    img = double(imread(trainingArray(k)));
+
+    trainingData(:, k) = reshape(img, [], 1);
+end
+
+% Initialize gallery image data
+[num_gallery_imgs, ~] = size(galleryImages);
+
+galleryImageData = zeros(92 * 112, num_gallery_imgs);
+
+% load in gallery image data
+for k = 1:num_gallery_imgs
+    img = double(imread(galleryImages(k)));
+
+    galleryImageData(:, k) = reshape(img, [], 1);
+end
+
+% Gather testing data information
+[num_testing_rows, ~] = size(testingArray);
+
+% Put whatever value of `k` that you want here
+[eigenvectors, img_avg] = PCA_covariance(trainingData, 200);
+
+% The i'th column stores the eigencoefficients of the k'th eigenvector
+% for the i'th gallery image.
+gallery_coeffs = eigenvectors' * galleryImageData;
+
+% a counter to keep track of how many times the system recognized the
+% face correctly.
+correct_counter = 0;
+
+% load in testing data
+for j = 1:num_testing_rows
+    % The correct face number
+    face_num = str2double(testingArray(j, 2));
+
+    testData = reshape(double(imread(testingArray(j, 1))), [], 1);
+    testData = testData - img_avg;
+
+    test_eigen_coeffs = eigenvectors' * testData;
+
+    gallery_coeffs_intermediate = gallery_coeffs - test_eigen_coeffs;
+
+    norms = zeros(num_gallery_imgs, 1);
+
+    for k = 1:num_gallery_imgs
+        norms(k) = norm(gallery_coeffs_intermediate(:, k));
+    end
+
+    [~, face_num_guess] = min(norms);
+
+    if face_num == face_num_guess
+        correct_counter = correct_counter + 1;
+    end
+end
