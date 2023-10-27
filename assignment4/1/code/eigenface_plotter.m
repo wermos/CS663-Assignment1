15c330165ff7ff661d85adcd2c52120606e31496
wermos <63574588+wermos@users.noreply.github.com>
2023-10-28 01:42:22 +0530

    Added a eigenface plotter and the generated images.

---
+++ assignment4/1/code/eigenface_plotter.m
@@ -0,0 +1,28 @@
+[trainingArray, ~, ~] = orl_path_loader();
+
+% Initialize training data array
+[num_training_rows, ~] = size(trainingArray);
+
+trainingData = zeros(92 * 112, num_training_rows);
+
+% load in training data
+for i = 1:num_training_rows
+    img = double(imread(trainingArray(i)));
+
+    trainingData(:, i) = reshape(img, [], 1);
+end
+
+[eigenfaces, ~] = PCA(trainingData, 25);
+
+tiledlayout(5, 5);
+
+for i = 1:25
+    img_data = reshape(eigenfaces(:, i), [112, 92]);
+
+    nexttile;
+
+    imshow(img_data, []);
+end
+
+exportgraphics(gca, "../images/eigenfaces.png");
+exportgraphics(gca, "../images/eigenfaces.pdf");
